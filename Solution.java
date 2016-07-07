97a157a8cce4711006952107196add6eac7c1eee
Brittany Kvanvig <brittany.kvanvig@utexas.edu>
2016-07-07 17:24:13 -0500

    removed excess data structure

--- Solution.java
+++ Solution.java
@@ -1,24 +1,22 @@
 import java.util.*;
 import java.io.*;
-
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> n1 = new HashMap<Integer, Integer>();
-        HashMap<Integer, Integer> res = new HashMap<Integer, Integer>();
+        Set<Integer> res = new HashSet<Integer>();
         for (int i = 0; i < nums1.length; i++){
             if (!n1.containsKey(Integer.valueOf(nums1[i]))){
                 n1.put(nums1[i], 0);
             }
         }
         for (int i = 0; i < nums2.length; i++){
-            if (n1.containsKey(Integer.valueOf(nums2[i])) && !res.containsKey(Integer.valueOf(nums2[i]))){
-                res.put(nums2[i], 0);
+            if (n1.containsKey(Integer.valueOf(nums2[i])) && !res.contains(Integer.valueOf(nums2[i]))){
+                res.add(Integer.valueOf(nums2[i]));
             }
         }
         int []sol = new int [res.size()];
         int ind = 0;
-        Set<Integer> keys = res.keySet();
-        for (Integer key : keys){
+        for (Integer key : res){
             sol[ind++] = key.intValue();
         }
         return sol;
